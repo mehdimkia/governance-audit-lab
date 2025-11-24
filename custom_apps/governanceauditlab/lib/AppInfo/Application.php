fdf27af3cf3ce0db2c9129b64280564688704c5b
mkia <mehdimx@gmail.com>
2025-11-24 12:14:45 +0100

    feat: register LabelMapper service for Governance Audit Lab

--- custom_apps/governanceauditlab/lib/AppInfo/Application.php
+++ custom_apps/governanceauditlab/lib/AppInfo/Application.php
@@ -8,6 +8,10 @@ use OCP\AppFramework\App;
 use OCP\AppFramework\Bootstrap\IBootContext;
 use OCP\AppFramework\Bootstrap\IBootstrap;
 use OCP\AppFramework\Bootstrap\IRegistrationContext;
+//our db connectors for labels
+use OCA\GovernanceAuditLab\Db\LabelMapper;
+use OCP\IDBConnection;
+
 
 class Application extends App implements IBootstrap {
 	public const APP_ID = 'governanceauditlab';
@@ -15,6 +19,12 @@ class Application extends App implements IBootstrap {
 	/** @psalm-suppress PossiblyUnusedMethod */
 	public function __construct() {
 		parent::__construct(self::APP_ID);
+		$container = $this->getContainer();
+		$container->registerService(LabelMapper::class, function($c) {
+			/** @var IDBConnection $db */
+			$db = $c->getServer()->get(IDBConnection::class);
+			return new LabelMapper($db);
+		});
 	}
 
 	public function register(IRegistrationContext $context): void {
