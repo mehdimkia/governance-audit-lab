cf883300b220215e2bf9a4c7d888fdff17979fc1
mkia <mehdimx@gmail.com>
2025-11-24 12:24:04 +0100

    feat: add debugLabels route to inspect labels

--- custom_apps/governanceauditlab/lib/Controller/PageController.php
+++ custom_apps/governanceauditlab/lib/Controller/PageController.php
@@ -12,10 +12,29 @@ use OCP\AppFramework\Http\Attribute\NoCSRFRequired;
 use OCP\AppFramework\Http\Attribute\OpenAPI;
 use OCP\AppFramework\Http\TemplateResponse;
 
+use OCA\GovernanceAuditLab\Db\Label;
+use OCA\GovernanceAuditLab\Db\LabelMapper;
+use OCP\AppFramework\Http\JSONResponse;
+use OCP\IRequest;
+
+
+
+
 /**
  * @psalm-suppress UnusedClass
  */
 class PageController extends Controller {
+
+	private LabelMapper $labelMapper;
+
+	public function __construct(
+		string $appName,
+		IRequest $request,
+		LabelMapper $labelMapper,
+	) {
+		parent::__construct($appName, $request);
+		$this->labelMapper = $labelMapper;
+	}
 	#[NoCSRFRequired]
 	#[NoAdminRequired]
 	#[OpenAPI(OpenAPI::SCOPE_IGNORE)]
@@ -25,5 +44,22 @@ class PageController extends Controller {
 			Application::APP_ID,
 			'index',
 		);
+	}
+	#[NoCSRFRequired]
+	#[NoAdminRequired]
+	#[OpenAPI(OpenAPI::SCOPE_IGNORE)]
+	#[FrontpageRoute(verb: 'GET', url: '/labels/debug')]
+	public function debugLabels(): JSONResponse {
+		$labels = $this->labelMapper->findAll();
+
+		$data = array_map(static function (Label $label) {
+			return [
+				'id'          => $label->getId(),
+				'name'        => $label->getName(),
+				'description' => $label->getDescription(),
+			];
+		}, $labels);
+
+		return new JSONResponse($data);
 	}
 }
